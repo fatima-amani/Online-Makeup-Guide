1d52c251d42767b4c9f0a4ca7788ebf23829b21a
fatima-amani <116259148+fatima-amani@users.noreply.github.com>
2023-02-18 22:21:46 +0530

    Update miniproject.c

--- miniproject.c
+++ miniproject.c
@@ -6,8 +6,15 @@ void FaceMakeup();
 void EyeMakeup();
 void Lipmakeup();
 
-void Foundation();  // please add all helper functions name here to summarise (category wise)
-void Powder();
+// Helper Functions for FaceMakeup()
+void Primer();
+void Foundation();  
+void Concealer();
+void LoosePowder();
+void Blush();
+void Contour();
+void Highlighter();
+void SettingSpray();
 
 struct UserDetail {
     char name[40];
@@ -392,28 +399,82 @@ void SettingSpray() {
 void EyeMakeup() {
     int choice;
         while(1) {
-        printf("heyy %s enter a category of eyemakeup \n
-        f for foudation // continue",user1.name);               // menu me choice vs option add add karde like 1. Facemakeup 2. eyemakeup
+        printf("heyy %s enter a category of eyemakeup \n 1. eyeliner 2. eyebrow definer 3. mascara 4. eyeshadow ",user->name);               // menu me choice vs option add add karde like 1. Facemakeup 2. eyemakeup
         scanf("%d",&choice);
         switch (choice)
         {
-        case 1 : EyeShadow();
+        case 1 : Eyeliner();
             break;
-        case 2 : Eyeliner();
+        case 2 : EyeBrowDef();
             break;
-        case 3 : EyeBrowDef();
+        case 3 : Mascara();
             break;
-        case 4 : Mascara();
-            break;
-        case 8 : return;
-            break;
-        case 9 : exit(0);
-
-        default: printf("enter a correct choice");
+        case 4 : EyeShadow();
+                break;
+        default: printf("\n the option you have entered does not exist, going back to main menu \n");
             break;
         }
     }
 }
+
+// Eye liner
+void EyeLiner() {
+    int choice,finish;
+    printf("%s, choose your preferred formulation  \n 1. retractable kajal 2.kohl 3. sketch liner 4. liquid liner 5. gel liner \n");
+    scanf("%d",&choice);
+    switch (choice)
+    {
+    case 1: printf("\n You can checkout :\n Maybelline New York The Collosal Kajal \n");      // everything printf in this format
+        
+              break;
+    case 2: 
+            printf("\n You can checkout :\n Maybelline New York Tattostudio Smokey \n"); 
+        
+        
+        break;
+    case 3: 
+        printf("\n You can checkout :\n Maybelline New York The Collosal Liner\n");
+        
+        
+        break;
+    case 4: while(1) {
+                printf("what is your perferred finish of liquid eyeliner 1. matte finish 2. glossy finish \n");
+                scanf("%d",&finish);
+                if(finish == 1) {
+                    printf("\n You can checkout :\n Maybelline New York Collosal Bold Eyeliner \n"); 
+                    break;
+                }
+                else if(finish == 2) {
+                    printf("\n You can checkout :\n Maybelline New York Hyper Glossy Liquid Liner \n"); 
+                    break;
+                }
+                else {
+                    printf("\n the option you have entered does not exist, please retry \n");
+                }
+            }   
+            break;
+    case 5: 
+        printf("\n You can checkout :\n Maybelline New York Lasting Drama Gel \n");
+        
+        break;
+    
+    default: printf("the option you have entered does not exist, going back to face makeup menu");
+        return;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 void LipMakeup() {
     int choice;
